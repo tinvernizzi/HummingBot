1502fa063f7b983160f60afba5bbbab314ec83bd
Zaki Berouk <zaki.berouk@etu.unice.fr>
2016-12-01 23:59:09 +0100

    Presentation added

---
+++ src/main/java/answer/Presentation.java
@@ -0,0 +1,26 @@
+package main.java.answer;
+
+import java.util.regex.*;
+
+/**
+ * Created by Zaki on 01/12/2016.
+ */
+public class Presentation {
+    private static Pattern pattern;
+    private static Matcher matcher;
+
+    public boolean matchesWith(String query) {
+        query.toLowerCase();
+
+        pattern = Pattern.compile("(hi) (hey) (who are you) (present yourself) (your name)");
+        matcher = pattern.matcher(query);
+        while(matcher.find()) {
+            return true;
+        }
+        return false;
+    }
+
+    public String getAnswer() {
+        return "Hi, I'm HummingBot, at your service.";
+    }
+}
