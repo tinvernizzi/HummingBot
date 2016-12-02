9536d78a3a51be91a49d113b5c850126d8049d4f
tinvernizzi <tanguy.invernizzi@yahoo.fr>
2016-12-02 05:37:17 +0100

    Added final

--- src/main/java/answer/whatIs.java
+++ src/main/java/answer/whatIs.java
@@ -5,9 +5,6 @@ import twitter4j.JSONException;
 
 import java.io.IOException;
 
-/**
- * Created by user on 02/12/2016.
- */
 public class whatIs implements Answer {
     private String word;
     private wikiQuery wiki;
@@ -18,9 +15,8 @@ public class whatIs implements Answer {
 
     public boolean matchesWith(String query) {
         if (query.matches("what is(.*)")
-                ||query.matches("the fuck is(.*)")
-                ||query.matches("tell me about(.*)"))
-        {
+                || query.matches("the fuck is(.*)")
+                || query.matches("tell me about(.*)")) {
             word = query;
             return true;
         }
@@ -28,13 +24,12 @@ public class whatIs implements Answer {
     }
 
     public String getAnswer() throws IOException, JSONException {
-        //traitement sur le bousin
-        if(word.equals("what is")){
+        if (word.equals("what is")) {
             return "Uh, it seems you forgot the end of your sentence :)";
         }
-        word = word.split("what is")[1].replaceAll("\\s","");
-        if(word.toLowerCase().equals("love")){
-            return "Don't hurt me";
+        word = word.split("what is")[1].replaceAll("\\s", "");
+        if (word.toLowerCase().equals("love")) {
+            return "❤ Don't hurt me ❤";
         }
         String answer = wiki.makeAQuery(word);
         return answer;
