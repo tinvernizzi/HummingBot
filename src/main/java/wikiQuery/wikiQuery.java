7020667795c57dbb0b1de41744e518bc69bea449
tinvernizzi <tanguy.invernizzi@yahoo.fr>
2016-12-02 02:54:57 +0100

    Added wikiQuery class - all credits go to Marc

---
+++ src/main/java/wikiQuery/wikiQuery.java
@@ -0,0 +1,32 @@
+package main.java.wikiQuery;
+
+import twitter4j.JSONArray;
+import twitter4j.JSONException;
+import twitter4j.JSONObject;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.Scanner;
+
+/**
+ * Created by user on 02/12/2016.
+ */
+public class wikiQuery {
+
+    public wikiQuery(){};
+
+    public String makeAQuery(String nameOfArticle) throws IOException, JSONException {
+        String out = new Scanner(new URL("https://en.wikipedia.org/w/api.php?action=opensearch&format=json&search=" + nameOfArticle).openStream(), "UTF-8").useDelimiter("\\A").next();
+        JSONArray JSONquery = new JSONArray(out);
+
+        JSONObject jo = new JSONObject();
+        // populate the array
+        jo.put("wikiQuery",JSONquery);
+
+        if (jo.getJSONArray("wikiQuery").get(1).toString().equals("[]") || jo.getJSONArray("wikiQuery").get(2).toString().equals("[\"\"]")) {
+            return null;
+        }
+        System.out.println(jo.getJSONArray("wikiQuery").get(2).toString().replaceAll("\",\"","\n"));
+        return "";
+    }
+}
