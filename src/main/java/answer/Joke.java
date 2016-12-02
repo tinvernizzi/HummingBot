22cd08423ac8be147cdbe56cbfb6dc13231e78a1
Charlotte Aknin <charlotte.aknin@etu.unice.fr>
2016-12-02 04:46:51 +0100

    adding jokes

--- src/main/java/answer/Joke.java
+++ src/main/java/answer/Joke.java
@@ -18,6 +18,16 @@ public class Joke implements Answer {
         this.jokes.add("A customer comes into the computer store. \n" +
                 "I'm looking for a mystery Adventure Game with lots of graphics. You know, something really challenging.\" \n" +
                 "\"Well,\" replied the clerk, \"Have you tried Vista?\"");
+        this.jokes.add("https://pbs.twimg.com/media/CyWYU08W8AEmjzx.jpg");
+        this.jokes.add("Why is the sky blue?\n" +
+                "Because if it was green you wouldn’t know where to stop mowing.");
+        this.jokes.add("What do Donald Trump and a Pumpkin have in common?\n" + "They’re orange on the outside, hollow on the inside, and responsible people will throw them away in early November.\n");
+        this.jokes.add("Why don't some couples go to the gym? Because some relationships don't work out.");
+        this.jokes.add("Did you hear about the guy who got hit in the head with a can of soda? He was lucky it was a soft drink.");
+        this.jokes.add("If there was someone selling drugs in this place, weed know.");
+        this.jokes.add("If you pay 25 cents to watch a bad football game don't expect your quarterback.");
+        this.jokes.add("I used to be a banker but I lost interest");
+        this.jokes.add("A prisoner's favorite punctuation mark is the period. It marks the end of his sentence.");
     }
 
     public boolean matchesWith(String query) {
