be3f1bbe9021e2fbb3cdec4acd9d586b91c0f90a
tinvernizzi <tanguy.invernizzi@yahoo.fr>
2016-12-02 03:10:57 +0100

    Added wikiQuery class - all credits go to Marc

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,11 @@
 package main.java;
 
-import twitter4j.*;
-import twitter4j.conf.ConfigurationBuilder;
 
-import java.util.List;
+import main.java.wikiQuery.wikiQuery;
+import twitter4j.JSONException;
+import twitter4j.TwitterException;
+
+import java.io.IOException;
 
 /**
  * Created by user on 01/12/2016.
@@ -12,25 +14,8 @@ public class Main {
 
     static Token tokenList;
 
-    public static void main (String[] args) throws TwitterException{
-        tokenList = new Token();
-        ConfigurationBuilder cb = new ConfigurationBuilder();
-        cb.setDebugEnabled(true)
-                .setOAuthConsumerKey(tokenList.getConsumerKey())
-                .setOAuthConsumerSecret(tokenList.getConsumerSecret())
-                .setOAuthAccessToken(tokenList.getAccessToken())
-                .setOAuthAccessTokenSecret(tokenList.getAccessTokenSecret());
-        TwitterFactory tf = new TwitterFactory(cb.build());
-        Twitter twitter = tf.getInstance();
-        Paging page = new Paging(1);
-        List<DirectMessage> directMessages;
-        directMessages = twitter.getDirectMessages(page);
-        for (DirectMessage message : directMessages) {
-            System.out.println("To: @" + message.getRecipientScreenName() + " id:" + message.getId() + " - "
-                    + message.getText());
-        }
-        DirectMessage message = twitter.sendDirectMessage("TanguyInvrnz", "TanguyInvrnz");
-        System.out.println("Direct message successfully sent to " + message.getRecipientScreenName());
-        System.exit(0);
+    public static void main (String[] args) throws TwitterException, JSONException, IOException {
+        wikiQuery wiki = new wikiQuery();
+        wiki.makeAQuery("frustration");
     }
 }
